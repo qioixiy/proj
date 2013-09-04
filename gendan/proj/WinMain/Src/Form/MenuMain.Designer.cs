325f125796a9f632fbb6c3d296969d85f742f2a9
qioixiy <qioixiy@gmail.com>
2013-09-04 22:30:54 +0800

    change MenuMain.style.

--- gendan/proj/WinMain/Src/Form/MenuMain.Designer.cs
+++ gendan/proj/WinMain/Src/Form/MenuMain.Designer.cs
@@ -191,7 +191,7 @@ namespace MenuMain
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button3);
             this.ForeColor = System.Drawing.Color.Black;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
             this.MaximizeBox = false;
